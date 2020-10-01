adf996cc4a790c09b54579d9d7e0c8c41025530f
Xavier Dutreilh <xavier@dutreilh.com>
2020-10-01 12:00:50 +0200

    Accept any npm version

--- src/root/__tests__/root.router.test.js
+++ src/root/__tests__/root.router.test.js
@@ -6,6 +6,6 @@ describe("GET /", () => {
   it("retrieves api information", async () => {
     const response = await request(api.callback()).get("/")
     expect(response.status).toBe(200)
-    expect(response.body).toEqual({ name: "gpus.midgar.fr", version: "1.0.0" })
+    expect(response.body).toEqual({ name: "gpus.midgar.fr", version: expect.any(String) })
   })
 })
