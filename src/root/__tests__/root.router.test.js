43830266f247d233dbee39fd7881f8ffa1b6a8f0
Xavier Dutreilh <xavier@dutreilh.com>
2020-10-01 12:16:46 +0200

    Reformat root test

--- src/root/__tests__/root.router.test.js
+++ src/root/__tests__/root.router.test.js
@@ -6,6 +6,9 @@ describe("GET /", () => {
   it("retrieves api information", async () => {
     const response = await request(api.callback()).get("/")
     expect(response.status).toBe(200)
-    expect(response.body).toEqual({ name: "gpus.midgar.fr", version: expect.any(String) })
+    expect(response.body).toEqual({
+      name: "gpus.midgar.fr",
+      version: expect.any(String),
+    })
   })
 })
