fa59fd56ca63395ac6060e0e4d6dd71eb0517cc9
Xavier Dutreilh <xavier@dutreilh.com>
2020-10-06 21:42:41 +0200

    Add Sentry client

--- src/api.js
+++ src/api.js
@@ -1,5 +1,8 @@
+const config = require("config")
 const Koa = require("koa")
 const logger = require("koa-logger")
+const Sentry = require("@sentry/node")
+const stripAnsi = require("strip-ansi")
 
 const offerRouter = require("./offer/offer.router")
 const rootRouter = require("./root/root.router")
@@ -7,7 +10,11 @@ const rootRouter = require("./root/root.router")
 const api = new Koa()
 
 if (process.env.NODE_ENV !== "test") {
-  api.use(logger())
+  api.use(
+    logger(str => {
+      console.log(stripAnsi(str))
+    })
+  )
 }
 
 api
@@ -15,5 +22,21 @@ api
   .use(rootRouter.allowedMethods())
   .use(offerRouter.routes())
   .use(offerRouter.allowedMethods())
+
+if (config.has("sentry.dsn")) {
+  Sentry.init({
+    dsn: config.get("sentry.dsn"),
+    environment: process.env.NODE_ENV,
+  })
+
+  api.on("error", (err, ctx) => {
+    Sentry.withScope(scope => {
+      scope.addEventProcessor(event => {
+        return Sentry.Handlers.parseRequest(event, ctx.request)
+      })
+      Sentry.captureException(err)
+    })
+  })
+}
 
 module.exports = api
