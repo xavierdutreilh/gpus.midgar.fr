cb29933eaeae03a1718aff1ed4c9e45bb91bd1f6
Xavier Dutreilh <xavier@dutreilh.com>
2020-10-09 09:06:59 +0200

    Normalize key ordering

--- config/default.js
+++ config/default.js
@@ -3,8 +3,8 @@ const { name, version } = require("../package")
 module.exports = {
   name,
   version,
+  port: 3000,
   database: {
     dsn: "postgres://gpus:gpus@database:5432/gpus",
   },
-  port: 3000,
 }
