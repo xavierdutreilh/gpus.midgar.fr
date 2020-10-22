f9b36cecef979e885d3900abaedecefe3ae94bb9
Xavier Dutreilh <xavier@dutreilh.com>
2020-10-22 19:42:39 +0200

    Reformat migration

--- db/migrations/20201022064544-create-tsmatch.js
+++ db/migrations/20201022064544-create-tsmatch.js
@@ -1,7 +1,7 @@
 module.exports = {
   up: async queryInterface => {
     await queryInterface.sequelize.query(
-      "CREATE FUNCTION tsmatch(a TSVECTOR, b TSQUERY) RETURNS BOOLEAN AS $$ BEGIN RETURN a @@ b; END; $$ LANGUAGE plpgsql;"
+      "CREATE FUNCTION tsmatch(a TSVECTOR, b TSQUERY) RETURNS BOOLEAN AS $$ BEGIN RETURN a @@ b; END; $$ LANGUAGE plpgsql"
     )
   },
   down: async queryInterface => {
