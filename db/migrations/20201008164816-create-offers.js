3e893b3bdf8784ae70d784150dbcd2122801375f
Xavier Dutreilh <xavier@dutreilh.com>
2020-10-22 19:29:36 +0200

    Wrap migration in transaction

--- db/migrations/20201008164816-create-offers.js
+++ db/migrations/20201008164816-create-offers.js
@@ -1,48 +1,58 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("offers", {
-      store: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        primaryKey: true,
-      },
-      key: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        primaryKey: true,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      price: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      status: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      url: {
-        type: Sequelize.STRING,
-      },
-      search: {
-        type: "TSVECTOR",
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
+    await queryInterface.sequelize.transaction(async transaction => {
+      await queryInterface.createTable(
+        "offers",
+        {
+          store: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            primaryKey: true,
+          },
+          key: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            primaryKey: true,
+          },
+          name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          price: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          status: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          url: {
+            type: Sequelize.STRING,
+          },
+          search: {
+            type: "TSVECTOR",
+          },
+          created_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+          },
+          updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+          },
+        },
+        { transaction }
+      )
+      await queryInterface.addIndex("offers", ["status"], { transaction })
+      await queryInterface.addIndex("offers", ["search"], {
+        using: "gin",
+        transaction,
+      })
+      await queryInterface.sequelize.query(
+        "CREATE TRIGGER offers_search_update BEFORE INSERT OR UPDATE ON offers FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger(search, 'pg_catalog.english', name)",
+        { transaction }
+      )
     })
-    await queryInterface.addIndex("offers", ["status"])
-    await queryInterface.addIndex("offers", ["search"], { using: "gin" })
-    await queryInterface.sequelize.query(
-      "CREATE TRIGGER offers_search_update BEFORE INSERT OR UPDATE ON offers FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger(search, 'pg_catalog.english', name)"
-    )
   },
   down: async queryInterface => {
     await queryInterface.dropTable("offers")
