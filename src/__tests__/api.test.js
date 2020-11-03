d7501ce384c3acc1caa2f03c818b078c2e855631
Xavier Dutreilh <xavier@dutreilh.com>
2020-11-03 18:38:33 +0100

    Test offer search

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -49,6 +49,13 @@ describe("GET /offers", () => {
       expect(response.body).toEqual([
         {
           store: "nvidia",
+          name: "NVIDIA GEFORCE RTX 3080",
+          price: "€719.00",
+          status: "unavailable",
+          url: null,
+        },
+        {
+          store: "nvidia",
           name: "NVIDIA GEFORCE RTX 3090",
           price: "€1,549.00",
           status: "unavailable",
@@ -56,10 +63,47 @@ describe("GET /offers", () => {
         },
         {
           store: "ldlc",
+          name: "ASUS GeForce ROG STRIX RTX 3080 O10G GAMING",
+          price: "€1,099.95",
+          status: "unavailable",
+          url: "https://www.ldlc.com/fiche/PB00375431.html",
+        },
+        {
+          store: "ldlc",
           name: "ASUS GeForce ROG STRIX RTX 3090 O24G GAMING",
           price: "€1,949.95",
           status: "unavailable",
           url: "https://www.ldlc.com/fiche/PB00369849.html",
+        },
+      ])
+    })
+  })
+})
+
+describe("GET /offers?store&name", () => {
+  describe("Accept: text/html", () => {
+    it("renders offers", async () => {
+      const response = await request(api.callback())
+        .get("/offers?store=nvidia&name=rtx+3090")
+        .set("Accept", "text/html")
+      expect(response.status).toBe(200)
+      expect(response.text).toMatchSnapshot()
+    })
+  })
+
+  describe("Accept: application/json", () => {
+    it("retrieves offers", async () => {
+      const response = await request(api.callback())
+        .get("/offers?store=nvidia&name=rtx+3090")
+        .set("Accept", "application/json")
+      expect(response.status).toBe(200)
+      expect(response.body).toEqual([
+        {
+          store: "nvidia",
+          name: "NVIDIA GEFORCE RTX 3090",
+          price: "€1,549.00",
+          status: "unavailable",
+          url: null,
         },
       ])
     })
